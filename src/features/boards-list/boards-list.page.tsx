8108378f74850683541f8a2151a0fde8fa87769b
TarAn <9986544@gmail.com>
2025-10-08 23:37:59 +0300

    env init

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,3 +1,4 @@
+import { CONFIG } from "@/shared/model/config";
 import { ROUTES } from "@/shared/model/routes";
 import { href, Link } from "react-router-dom";
 
@@ -5,6 +6,7 @@ function BoardsListPage() {
   return (
     <div>
       <h1>Boards list</h1>
+      <h2>{CONFIG.API_BASE_URL}</h2>
       <Link to={href(ROUTES.BOARD, { boardId: '1'})}>Board 1</Link>
     </div>
   );
